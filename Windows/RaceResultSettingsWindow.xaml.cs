701b3ae110b816c467a5560a8e6ec2c3f341aaac
Marusko <matussusky@gmail.com>
2026-02-02 12:55:00 +0100

    Added code behind for RR settings window

--- Windows/RaceResultSettingsWindow.xaml.cs
+++ Windows/RaceResultSettingsWindow.xaml.cs
@@ -10,15 +10,38 @@ namespace TimeMaker.Windows
         public RaceResultSettingsWindow()
         {
             InitializeComponent();
+            App.RaceResult.RaceResultApiLoaded += OnRaceResultApiLoaded;
         }
 
-        private void Load(object sender, RoutedEventArgs e)
+        private void OnRaceResultApiLoaded(object? sender, Models.RaceResultApiLoadedEventArgs e)
         {
-
+            Dispatcher.Invoke(() =>
+            {
+                PointsApiLabel.Content = e.PointsApiStatus;
+                RawDataApiLabel.Content = e.ManualApiStatus;
+                BibListApiLabel.Content = e.BibsApiStatus;
+            });
         }
 
-        private void Save(object sender, RoutedEventArgs e)
+        private async void Load(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                await App.RaceResult.LoadApi(ApiLinkText.Text);
+                SaveButton.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBoxButton button = MessageBoxButton.OK;
+                MessageBoxImage icon = MessageBoxImage.Error;
+                MessageBox.Show($"Nastala chyba pri načítavaní API: {ex.Message}", "Chyba", button, icon, MessageBoxResult.OK);
+            }
+        }
+
+        private async void Save(object sender, RoutedEventArgs e)
+        {
+            await App.RaceResult.Start();
+            App.RaceResult.RaceResultApiLoaded -= OnRaceResultApiLoaded;
             Close();
         }
     }
