41c2016cdc22a58380d5c32b2e49745530b400bd
Marusko <matussusky@gmail.com>
2026-02-02 14:49:04 +0100

    Completed source manager service

--- Services/SourceManagerService.cs
+++ Services/SourceManagerService.cs
@@ -1,13 +1,46 @@
 ﻿using System.Collections.Concurrent;
+using TimeMaker.Models;
 
 namespace TimeMaker.Services
 {
     public class SourceManagerService : IDisposable
     {
         public ConcurrentDictionary<string, SourceService> Sources { get; private set; } = new();
+
+        public SourceService? GetSource(string key)
+        {
+            return Sources.GetValueOrDefault(key);
+        }
+
+        public bool AddSource(Type sourceType, SourceInitModel initModel)
+        {
+            if (sourceType == typeof(FileService))
+            {
+                var fileService = new FileService();
+                fileService.Init(initModel);
+                return Sources.TryAdd(fileService.Id, fileService);
+            }
+            else if (sourceType == typeof(SerialPortService))
+            {
+                var serialPortService = new SerialPortService();
+                serialPortService.Init(initModel);
+                return Sources.TryAdd(serialPortService.Id, serialPortService);
+            }
+
+            return false;
+        }
+
+        public bool RemoveSource(string id)
+        {
+            return Sources.TryRemove(id, out _);
+        }
+
         public void Dispose()
         {
-            // TODO release managed resources here
+            foreach (var ss in Sources)
+            {
+                ss.Value.Stop();
+            }
         }
     }
 }
