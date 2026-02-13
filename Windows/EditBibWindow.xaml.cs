557743b7414c4b1aecd6acac6a567dba55a53d1f
Marusko <matussusky@gmail.com>
2026-02-13 18:46:13 +0100

    Added window for changing bib number

---
+++ Windows/EditBibWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace TimeMaker.Windows
+{
+    /// <summary>
+    /// Interaction logic for EditBibWindow.xaml
+    /// </summary>
+    public partial class EditBibWindow
+    {
+        private string _bib;
+        public EditBibWindow(string bib)
+        {
+            InitializeComponent();
+            _bib = bib;
+            BibText.Text = _bib;
+        }
+
+        public string GetBib()
+        {
+            return _bib;
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            _bib = BibText.Text;
+            Close();
+        }
+    }
+}
