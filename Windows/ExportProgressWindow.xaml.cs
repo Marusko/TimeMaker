2739c61bfa87162266c93f95179ab31c6661dfc7
Marusko <matussusky@gmail.com>
2026-02-27 18:46:27 +0100

    Added support windows for exporting data

---
+++ Windows/ExportProgressWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace TimeMaker.Windows
+{
+    /// <summary>
+    /// Interaction logic for ExportProgressWindow.xaml
+    /// </summary>
+    public partial class ExportProgressWindow
+    {
+        public ExportProgressWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Report(int value)
+        {
+            ProgressBar.Value = value;
+        }
+    }
+}
---
+++ Windows/ExportSeparatorWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TimeMaker.Windows
+{
+    /// <summary>
+    /// Interaction logic for ExportSeparatorWindow.xaml
+    /// </summary>
+    public partial class ExportSeparatorWindow
+    {
+        private char _delimiter = ';';
+        public ExportSeparatorWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DelimiterChecked(object sender, RoutedEventArgs e)
+        {
+            if (sender is RadioButton button)
+            {
+                _delimiter = button.Content.ToString()?[0] ?? ';';
+            }
+        }
+
+        public char GetDelimiter()
+        {
+            return _delimiter;
+        }
+
+        private void Export(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
